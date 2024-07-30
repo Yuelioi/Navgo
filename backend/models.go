92a2723c3432ed05829fe1dc7cafeb2a48a4c545
yuelioi <yuelioi1210@gmail.com>
2024-07-30 19:42:55 +0800

    模型增加下载信息字段

--- backend/models.go
+++ backend/models.go
@@ -3,13 +3,26 @@ package main
 import "time"
 
 type Category struct {
-	Name        string
-	DisplayName string
-	Icon        string
+	Name  string
+	Title string
+	Icon  string
 }
+
+type DownloadInfo struct {
+	Version     string
+	SizeKb      int
+	UpdatedAt   time.Time // 更新时间
+	IsOfficial  bool      // 是否为官方
+	Description string    // 描述
+
+}
+
 type Nav struct {
 	ID          string     // 唯一标识符
+	Name        string     // 导航名称(链接路径)
 	Title       string     // 导航标题
+	IsOfficial  bool       // 是否为官方
+	Alias       []string   // 其他别名
 	OfficialURL []string   // 官网地址
 	DocsURL     []string   // 文档地址
 	DownloadURL []string   // 下载地址
